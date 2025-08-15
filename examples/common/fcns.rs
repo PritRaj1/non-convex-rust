bf0ff1f35de2a55e0a72b9f0b7811537e6ea668f
PritRaj1 <prithvi@zettalaboratories.com>
2025-08-15 12:00:38 -0700

    obj fix

--- examples/common/fcns.rs
+++ examples/common/fcns.rs
@@ -58,26 +58,27 @@ where
     DefaultAllocator: Allocator<U2>,
 {
     fn f(&self, x: &SVector<f64, 2>) -> f64 {
-        let x1 = x[0];
-        let x2 = x[1];
+        let gaussian1 = -0.5 * ((x[0] - 3.0).powi(2) + (x[1] - 3.0).powi(2));
+        let gaussian2 = -0.3 * ((x[0] - 7.0).powi(2) + (x[1] - 7.0).powi(2));
+        let gaussian3 = -0.2 * ((x[0] - 7.0).powi(2) + (x[1] - 3.0).powi(2));
         
-        10.0 * (-((x1 - 3.0).powi(2) + (x2 - 3.0).powi(2)) / 0.5).exp() +
-        5.0 * (-((x1 - 7.0).powi(2) + (x2 - 7.0).powi(2)) / 0.3).exp() +
-        5.0 * (-((x1 - 7.0).powi(2) + (x2 - 3.0).powi(2)) / 0.2).exp()
+        10.0 * gaussian1.exp() + 5.0 * gaussian2.exp() + 5.0 * gaussian3.exp()
     }
 
     fn gradient(&self, x: &SVector<f64, 2>) -> Option<SVector<f64, 2>> {
-        let x1 = x[0];
-        let x2 = x[1];
         let mut grad = SVector::<f64, 2>::zeros();
         
-        grad[0] = 10.0 * (-2.0 * (x1 - 3.0) / 0.5) * (-((x1 - 3.0).powi(2) + (x2 - 3.0).powi(2)) / 0.5).exp() +
-                  5.0 * (-2.0 * (x1 - 7.0) / 0.3) * (-((x1 - 7.0).powi(2) + (x2 - 7.0).powi(2)) / 0.3).exp() +
-                  5.0 * (-2.0 * (x1 - 7.0) / 0.2) * (-((x1 - 7.0).powi(2) + (x2 - 3.0).powi(2)) / 0.2).exp();
+        let exp1 = (-0.5 * ((x[0] - 3.0).powi(2) + (x[1] - 3.0).powi(2))).exp();
+        grad[0] += 10.0 * exp1 * (-2.0 * 0.5 * (x[0] - 3.0));
+        grad[1] += 10.0 * exp1 * (-2.0 * 0.5 * (x[1] - 3.0));
         
-        grad[1] = 10.0 * (-2.0 * (x2 - 3.0) / 0.5) * (-((x1 - 3.0).powi(2) + (x2 - 3.0).powi(2)) / 0.5).exp() +
-                  5.0 * (-2.0 * (x2 - 7.0) / 0.3) * (-((x1 - 7.0).powi(2) + (x2 - 7.0).powi(2)) / 0.3).exp() +
-                  5.0 * (-2.0 * (x2 - 3.0) / 0.2) * (-((x1 - 7.0).powi(2) + (x2 - 3.0).powi(2)) / 0.2).exp();
+        let exp2 = (-0.3 * ((x[0] - 7.0).powi(2) + (x[1] - 7.0).powi(2))).exp();
+        grad[0] += 5.0 * exp2 * (-2.0 * 0.3 * (x[0] - 7.0));
+        grad[1] += 5.0 * exp2 * (-2.0 * 0.3 * (x[1] - 7.0));
+        
+        let exp3 = (-0.2 * ((x[0] - 7.0).powi(2) + (x[1] - 3.0).powi(2))).exp();
+        grad[0] += 5.0 * exp3 * (-2.0 * 0.2 * (x[0] - 7.0));
+        grad[1] += 5.0 * exp3 * (-2.0 * 0.2 * (x[1] - 3.0));
         
         Some(grad)
     }
